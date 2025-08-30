154d15ec4488594cfde70ac513711497d8d4ff01
Tal Kohavy <tal.kohavy@controlup.com>
2025-08-30 18:04:55 +0300

    using rollup instead of esbuild

--- build.config.mjs
+++ build.config.mjs
@@ -1,5 +1,4 @@
 import { execSync } from 'child_process';
-import { build as esbuild } from 'esbuild';
 import fs, { cpSync } from 'fs';
 import os from 'os';
 import path from 'path';
@@ -20,8 +19,6 @@ import path from 'path';
  */
 
 const ROOT_PROJECT = process.cwd();
-const mode = process.env.NODE_ENV;
-const isProd = mode === 'production';
 const outDirName = 'dist';
 const COLORS = {
   green: '\x1b[32m',
@@ -38,7 +35,7 @@ async function buildPackageConfig() {
 
   cleanTargetDirectory(outDirName);
 
-  await build(outDirName);
+  build();
 
   copyStaticFiles(outDirName);
 
@@ -61,35 +58,9 @@ function cleanTargetDirectory(outDirName) {
   console.log('');
 }
 
-/**
- * @param {string} outDirName
- */
-async function build(outDirName) {
-  console.log(`${COLORS.green}- Step 2:${COLORS.stop} build the output dir`);
-
-  await esbuild({
-    entryPoints: ['src/index.ts'],
-    bundle: true,
-    outfile: `${outDirName}/index.js`,
-    sourcemap: !isProd, // <--- defaults to `false`. for 'node', create sourcemaps is for development only.
-    minify: isProd, // <--- defaults to `false`. should be `true` only in production.
-    platform: 'node', // <--- defaults to 'browser'. If you're creating a CLI tool, use 'node' value. Setting platform to 'node' is beneficial when for example, all packages that are built-in to node such as fs are automatically marked as external so esbuild doesn't try to bundle them.
-    format: 'cjs', // <--- When platform is set to 'node', this defaults to 'cjs'.
-    tsconfig: 'tsconfig.json', // <--- Normally the build API automatically discovers tsconfig.json files and reads their contents during a build. However, you can also configure a custom tsconfig.json file to use instead. This can be useful if you need to do multiple builds of the same code with different settings.
-    treeShaking: true, // <--- defaults to `true`. Removes dead code.
-    mainFields: ['main', 'module'], // <--- When platform is set to 'node', this defaults to 'module','main'. When platform is set to 'browser', this defaults to 'browser','module','main'. IMPORTANT! The order matters! 'main', 'module' is not the same as 'module', 'main'! I chose the more risky one, that attempts to tree-shake, but could potentially fail.
-    packages: 'external', // <--- You also may not want to bundle your dependencies with esbuild. There are many node-specific features that esbuild doesn't support while bundling such as __dirname, import.meta.url, fs.readFileSync, and *.node native binary modules. You can exclude all of your dependencies from the bundle by setting packages to external. If you do this, your dependencies must still be present on the file system at run-time since they are no longer included in the bundle.
-    conditions: [], // <--- If no custom conditions are configured, the Webpack-specific module condition is also included. The module condition is used by package authors to provide a tree-shakable ESM alternative to a CommonJS file without creating a dual package hazard. You can prevent the module condition from being included by explicitly configuring some custom conditions (even an empty list).
-    // keepNames: true, // <--- defaults to `false`. If you want to keep the names of the original files, set this to `true`. This is useful for debugging.
-    /**
-     * Some npm packages you want to use may not be designed to be run in the browser.
-     * Sometimes you can use esbuild's configuration options to work around certain issues and successfully
-     * bundle the package anyway. Undefined globals can be replaced with either the define feature in
-     * simple cases or the inject feature in more complex cases.
-     */
-    // define :
-    // inject :
-  });
+function build() {
+  console.log(`${COLORS.green}- Step 2:${COLORS.stop} build`);
+  execSync('rollup --config'); // or the full command: rollup --config rollup.config.mjs
 
   console.log('');
 }
---
+++ rollup.config.mjs
@@ -0,0 +1,23 @@
+import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.cjs.cjs',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.esm.mjs',
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [typescript(), terser()],
+};
