bbd13318e8c56b52668dd427f8b1acf51274bd73
Tal Kohavy <tal.kohavy@controlup.com>
2025-08-30 18:09:28 +0300

    updated the example index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('lucky-server');
+export type User = {
+  id: number;
+  name: string;
+  email: string;
+};
+
+export function doSomething(): void {
+  const user: User = { id: 1, name: 'John Doe', email: 'tal@gmail.com' };
+
+  console.log('user is:', user);
+  console.log('package template');
+}
